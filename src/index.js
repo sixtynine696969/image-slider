a69c4366d0b4100a0d61f19a9853e72107ab5f6c
sixtynine696969 <134623948+sixtynine696969@users.noreply.github.com>
2023-09-01 20:51:12 +0200

    Add functionality for nav circles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 
 const slideButtons = document.querySelectorAll('.slider > button');
+const slideCircles = document.querySelectorAll('.nav-circle');
 
 function doesNextImageExist(index) {
     return document.querySelector(`img[data-image-index="${index + 1}"]`);
@@ -41,5 +42,25 @@ slideButtons.forEach(btn => {
         }
 
         moveToImage(visibleImage, selectedCircle, nextIndex);
+    })
+})
+
+function unselectAndHide() {
+    document.querySelector('.visible').classList.remove('visible');
+    document.querySelector('.selected').classList.remove('selected');
+}
+
+function selectAndShow(index) {
+    document.querySelector(`img[data-image-index="${index}"]`).classList.add('visible');
+    document.querySelector(`button[data-image-nav="${index}"]`).classList.add('selected');
+}
+
+slideCircles.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        const selectedCircle = e.target;
+        const selectedCircleIndex = Number(selectedCircle.dataset.imageNav);
+
+        unselectAndHide();
+        selectAndShow(selectedCircleIndex);
     })
 })
