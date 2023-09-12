b66ae955a37ed3b86e738505c090813e80f52445
sixtynine696969 <134623948+sixtynine696969@users.noreply.github.com>
2023-09-12 07:47:09 +0200

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: [
-              ['@babel/preset-env', { targets: "defaults" }]
+              ['@babel/preset-env', { targets: 'defaults' }],
             ],
           },
         },
